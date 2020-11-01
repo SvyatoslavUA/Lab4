db6a0c2324206cbbe37f1d6ac553d586fdbc1c2e
Svyatoslavua <svyatika08@gmail.com>
2020-11-01 18:56:04 +0200

    Laba4.1

---
+++ Laba4.1/Laba4.1/Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int i;
+	double S;
+	S = 0;
+	i = 1;
+	while (i <= 15)
+	{
+		S += (sin(10 * i) + (cos(10 / i))) / sqrt(i);
+		i++;
+	}
+	cout << S << endl;
+	S = 0;
+	i = 1;
+	do {
+		S += (sin(10 * i) + (cos(10 / i))) / sqrt(i);
+		i++;
+	} while (i <= 15);
+	cout << S << endl;
+	S = 0;
+	for (i = 1; i <= 15; i++)
+	{
+		S += (sin(10 * i) + (cos(10 / i))) / sqrt(i);
+	}
+	cout << S << endl;
+	S = 0;
+	for (i = 15; i >= 1; i--)
+	{
+		S += (sin(10 * i) + (cos(10 / i))) / sqrt(i);
+	}
+	cout << S << endl;
+	return 0;
+}

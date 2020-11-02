7b9a311abdad22fb3d1d80a89cde3cda749e598e
SvyatoslavUA <72656919+SvyatoslavUA@users.noreply.github.com>
2020-11-02 18:11:07 +0200

    Add files via upload

--- Laba4.5/Laba4.5/Source.cpp
+++ Laba4.5/Laba4.5/Source.cpp
@@ -6,12 +6,12 @@ int main()
 {	
     double x, y, R;
 	srand((unsigned)time(NULL));
+	cout << "R = "; cin >> R;
 	for (int i = 0; i < 10; i++)
 	{
 		cout << "x = "; cin >> x;
 		cout << "y = "; cin >> y;
-		cout << "R = "; cin >> R;
-		if (((x * x + y * y) <= R * R && x <= 0 && y <= 0) ||
+		 R;if (((x * x + y * y) <= R * R && x <= 0 && y <= 0) ||
 			(y >= (x - 1) * (x - 1) && x >= 0 && (x * x + y * y) <= R * R))
 			cout << "yes" << endl;
 		else
@@ -20,7 +20,6 @@ int main()
 		cout << endl << fixed;
 	for (int i = 0; i < 10; i++)
 	{
-		cout << "R = "; cin >> R;
 		x = -R + rand() * (2 * R) / RAND_MAX;
 		y = -R + rand() * (2 * R) / RAND_MAX;
 			if (((x * x + y * y) <= R * R && x <= 0 && y <= 0) ||
